64ae2d8f2633a6e26f4a3e399f553de7a9dc6727
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-08-10 15:47:21 -0300

    first cmmit

---
+++ app/src/main/java/me/dio/teatro_marcelo_denny_app/MainActivity.kt
@@ -0,0 +1,29 @@
+package me.dio.teatro_marcelo_denny_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnSetEvent = findViewById<Button>(R.id.btn_setEvent)
+        btnSetEvent.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                .putExtra(TITLE, "Oficina de Teatro Narrativo")
+                .putExtra(EVENT_LOCATION, "Teatro Macelo Denny")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis()+(60*60*1000))
+
+            startActivity(intent)
+        }
+
+    }
+}
